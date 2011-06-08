c51ff4cd417b6d4a0053330332f40f32c6149f8b
Kirsten Comandich <kcomandich@gmail.com>
2011-06-08 15:56:07 -0700

    Added Mozilla as Advocate-level sponsor on home page. Reordered Advocate-level sponsors to reflect amount of money they've donated.

--- frontpage.php
+++ frontpage.php
@@ -37,11 +37,12 @@ Template Name: Front Page
               <h3><a href="/sponsors/2011/">2011 Sponsors</a></h3>
               <h4>Advocates</h4>
               <ul>
-                <li><a href="http://myemma.com/"><img src='http://opensourcebridge.org/wp-content/uploads/2009/02/emma_logo09.png' alt='emma' width="170" /></a></li>
+                <li><a href='http://www.mozilla.com/'><img src='http://opensourcebridge.org/wp-content/uploads/2010/05/mozilla.jpg' alt='Mozilla' width="170" /></a></li>
+                <li><a href='http://www.google.com/'><img src='<?php bloginfo('template_directory'); ?>/i/sponsors/google.png' alt='Google' /></a></li>
                 <li><a href="http://www.gilt.com"><img src='http://opensourcebridge.org/wp-content/uploads/2011/05/Gilt_Groupe.jpg' alt='Gilt Groupe' width="170" /></a></li>
                 <li><a href="http://openstack.org"><img src='http://opensourcebridge.org/wp-content/uploads/2011/05/openstack.gif' alt='OpenStack' width="170" /></a></li>
                 <li><a href="http://www.discogs.com/"><img src='http://opensourcebridge.org/wp-content/uploads/2011/05/discogs.jpg' alt='Discogs' width="170" /></a></li>
-                <li><a href='http://www.google.com/'><img src='<?php bloginfo('template_directory'); ?>/i/sponsors/google.png' alt='Google' /></a></li>
+                <li><a href="http://myemma.com/"><img src='http://opensourcebridge.org/wp-content/uploads/2009/02/emma_logo09.png' alt='emma' width="170" /></a></li>
               </ul>
               <h4>Citizens</h4>
               <ul>
