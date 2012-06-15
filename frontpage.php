855acadcfd845a6a7178ab245536329e38795c47
Kirsten Comandich <kcomandich@gmail.com>
2012-06-15 14:35:21 -0700

    Added Gilt, New Relic and Bluebox as sponsors on the front page.

--- frontpage.php
+++ frontpage.php
@@ -55,11 +55,14 @@ Template Name: Front Page
                 <li><a href='http://www.mozilla.com/'><img src='http://opensourcebridge.org/wp-content/uploads/2010/05/mozilla.jpg' alt='Mozilla' width="170" /></a></li>
                 <li><a href="http://twitter.com/"><img src='http://opensourcebridge.org/wp-content/uploads/2012/04/twitter.170.png' alt='Twitter' width="170" /></a></li>
                 <li><a href="http://www.discogs.com/"><img src='http://opensourcebridge.org/wp-content/uploads/2011/05/discogs.jpg' alt='Discogs' width="170" /></a></li>
+                <li><a href="http://www.gilt.com/careers/tech/category/tech-department/about-tech/"><img src='http://opensourcebridge.org/wp-content/uploads/2011/05/Gilt_Groupe.jpg' alt='Gilt Groupe' width="170" /></a></li>
               </ul>
               <h4>Friends</h4>
               <ul>
                 <li><a href='http://www.elevatedcode.com/'><img src='http://opensourcebridge.org/wp-content/uploads/2012/03/elevatedcode.170.png' alt='Elevated Code' width="170" /></a></li>
                 <li><a href='http://www.thoughtworks.com/'><img src='http://opensourcebridge.org/wp-content/uploads/2012/05/thoughtworks.170.png' alt='Thoughtworks' width="170" /></a></li>
+                <li><a href='http://newrelic.com/about/jobs'><img src='http://opensourcebridge.org/wp-content/uploads/2012/06/newrelic.170.png' alt='New Relic' width="170" /></a></li>
+                <li><a href='http://www.bluebox.net/'><img src='http://opensourcebridge.org/wp-content/uploads/2012/06/bluebox.170.png' alt='Bluebox' width="170" /></a></li>
               </ul>
               <h4>Hosting</h4>
               <ul>
